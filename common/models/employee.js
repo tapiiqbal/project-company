137b4160b58e0e85b5cbdaab77f26b91df80a8b6
iM-yOUNG <tapiiqbal@gmail.com>
2019-07-24 13:35:19 +0700

    second project

--- common/models/employee.js
+++ common/models/employee.js
@@ -3,6 +3,81 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-module.exports = function(employee) {
+module.exports = function(Employee) {
+	//create path
+	Employee.remoteMethod(
+		'getByFirstName',
+		{
+			description: 'get firsname like -> Muhammad',
+			accepts: [
+				{arg: 'First Name', type: 'string'}
+			],
+			returns: {
+				arg: 'res',type: 'object', root: true		
+			},
+			http: { path: '/getByFirstName', verb: 'get'}
+		}
+	);
+//promise untuk menghilangkan asyncronus
+//reject, resolve adalah annotation
 
-};
+	Employee.getByFirstName = function(first_name, callback){
+		new Promise(function(resolve, reject){
+
+			var filter = {
+				where : {
+					first_name : {
+						like : first_name
+					}
+				}
+			}
+			Employee.find(filter, function(err, result){
+				if (err) reject (err)
+					if (result === null) {
+						err = new Error('Cannot find that name')
+						err.statusCode = 404
+						reject(err)
+					}	
+
+					resolve(result)
+			})
+		}).then(function(res){
+			if (!res) callback (err)
+				return callback(null,res)
+		}).catch(function(err){
+			callback (err);
+		})	
+	}
+//promise untuk menghilangkan asyncronus
+//reject, resolve adalah annotation
+
+	Employee.getByFirstId= function(first_name, callback){
+		new Promise(function(resolve, reject){
+
+			var filter = {
+				where : {
+					first_name : {
+						like : first_name
+					}
+				}
+			}
+			Employee.find(filter, function(err, result){
+				if (err) reject (err)
+					if (result === null) {
+						err = new Error('Cannot find that name')
+						err.statusCode = 404
+						reject(err)
+					}	
+
+					resolve(result)
+			})
+		}).then(function(res){
+			if (!res) callback (err)
+				return callback(null,res)
+		}).catch(function(err){
+			callback (err);
+		})	
+	}
+}
+
+
